3ef4de511d6a1a2b971731e84037694829569fa0
RelebohileMolefe2 <164934121+RelebohileMolefe2@users.noreply.github.com>
2025-03-29 00:49:04 +0200

    Add files via upload

---
+++ src/test/java/za/ac/cput/repository/UserRepositoryTest.java
@@ -0,0 +1,36 @@
+package za.ac.cput.repository;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+class UserRepositoryTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void getInstance() {
+    }
+
+    @Test
+    void create() {
+    }
+
+    @Test
+    void read() {
+    }
+
+    @Test
+    void update() {
+    }
+
+    @Test
+    void delete() {
+    }
+
+    @Test
+    void getAll() {
+    }
+}
